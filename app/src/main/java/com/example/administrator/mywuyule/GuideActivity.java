f44f89a0fae8873e0f021fe9c7e1c13cf745ad3e
liudehua <846685444@qq.com>
2016-10-08 23:54:04 +0800

    第一次上传
    实验用法

--- app/src/main/java/com/example/administrator/mywuyule/GuideActivity.java
+++ app/src/main/java/com/example/administrator/mywuyule/GuideActivity.java
@@ -1,15 +1,16 @@
 package com.example.administrator.mywuyule;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Administrator on 2016/10/8.
+ */
+public class GuideActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this,GuideActivity.class));
     }
 }
